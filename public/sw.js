bfad0fc0c5744e93c4ad1a86364bfd661a3a82e1
bershin <bershin@gmail.com>
2026-04-20 11:31:31 +0100

    work for ipad

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("push", (event) => {
+  if (event.data) {
+    const data = event.data.json();
+    const options = {
+      body: data.body,
+      icon: data.icon || "/icon-192x192.png",
+      badge: "/icon-192x192.png",
+      vibrate: [100, 50, 100],
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+  }
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow("/dashboard"));
+});
